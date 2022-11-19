89962ddb08ba4ab8b8e7422e32fb11acb5445fdb
L2HYUNN <donghyuni98@gmail.com>
2022-11-19 13:23:51 +0900

    :sparkles: add recoil setting in _app.tsx
    리코일 사용을 위한 기본 설정 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />;
+      </RecoilRoot>
     </QueryClientProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
