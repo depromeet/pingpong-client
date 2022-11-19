42d8993dbbd287e294a2b794b237bb385bbc609b
L2HYUNN <donghyuni98@gmail.com>
2022-11-19 23:29:44 +0900

    :recycle: refactor: fix lint error
    lint룰을 적용하여 잘못된 파일들 수정

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import './button.css';
+
+import React from 'react';
 
 interface ButtonProps {
   /**
@@ -27,13 +28,7 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
+export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
