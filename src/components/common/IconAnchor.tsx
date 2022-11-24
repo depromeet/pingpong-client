a05310598c548ec62e916f4d9386d8ef918fa2e6
L2HYUNN <donghyuni98@gmail.com>
2022-11-25 00:59:52 +0900

    :recycle: refactor(IconAnchorProps): Change IcnAnchor type name

--- src/components/common/IconAnchor.tsx
+++ src/components/common/IconAnchor.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import type { AnchorHTMLAttributes } from 'react';
 
-interface IconAnchorOnlyProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+interface IconAnchorProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   icon: JSX.Element;
   href: string;
   className?: string;
 }
 
-const IconAnchor = ({ icon, href, className, ...props }: IconAnchorOnlyProps) => {
+const IconAnchor = ({ icon, href, className, ...props }: IconAnchorProps) => {
   return (
     <Link href={href} {...props} className={className}>
       {icon}
