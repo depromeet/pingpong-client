51fa0378ad665b66a75bd3a41432f22bb9c5ce17
L2HYUNN <donghyuni98@gmail.com>
2022-11-26 16:06:16 +0900

    :recycle: refactor(NavigationBar): Remove NavigationBarList props

--- src/components/common/NavigationBarList.tsx
+++ src/components/common/NavigationBarList.tsx
@@ -1,14 +1,17 @@
+import { ProfileIcon, TalentRegisterIcon, TalentSearchIcon } from 'src/components/common/icons';
+
 import IconAnchor from './IconAnchor';
-import type { NavigationBarListItemProps } from './NavigationBar';
 
-interface NavigationBarListProps {
-  items: NavigationBarListItemProps;
-}
+const NAV_LIST_INFO = [
+  { icon: <TalentSearchIcon />, href: '/talent/search', uuid: '1', text: '재능 탐색' },
+  { icon: <TalentRegisterIcon />, href: '/talent/register', uuid: '2', text: '재능 등록' },
+  { icon: <ProfileIcon />, href: '/profile', uuid: '3', text: '프로필' },
+];
 
-const NavigationBarList = ({ items }: NavigationBarListProps) => {
+const NavigationBarList = () => {
   return (
     <>
-      {items.map(({ icon, href, uuid, text }) => {
+      {NAV_LIST_INFO.map(({ icon, href, uuid, text }) => {
         return (
           <li key={uuid} className="w-max">
             <label className="flex-col justify-center align-middle">
