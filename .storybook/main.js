cc8f617efb549d7115889c751381c56494f25440
doyoonear <doyoonleee@gmail.com>
2022-11-26 12:21:25 +0900

    :bug: fix: Fix svg import error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -16,5 +19,17 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: (config) => {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config;
   },
 };
