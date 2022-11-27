23b5a6b93323ccf69db8e20eee30e26b5ba16c12
L2HYUNN <donghyuni98@gmail.com>
2022-11-27 22:41:32 +0900

    :sparkles: feat(storybook): Add NavigationBar Stroybook

---
+++ src/components/common/NavigationBar.stories.tsx
@@ -0,0 +1,15 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import NavigationBar from './NavigationBar';
+
+export default {
+  title: 'NavigationBar',
+  component: NavigationBar,
+} as ComponentMeta<typeof NavigationBar>;
+
+const Template: ComponentStory<typeof NavigationBar> = () => {
+  return <NavigationBar />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
