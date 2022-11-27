887c9b0c2b22fbde664166f8d4ee86731b8c1706
L2HYUNN <donghyuni98@gmail.com>
2022-11-27 22:38:11 +0900

    :recycle: refactor(gnb): Apply design guide style
    design guide 스타일 적용

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -10,14 +10,14 @@ const NAV_LIST_INFO = [
 
 const NavigationBar = () => {
   return (
-    <nav className="w-[375px]">
-      <ul className="flex w-full justify-evenly">
+    <nav className="bg-primary-dark w-full h-[82px]">
+      <ul className="grid grid-cols-3">
         {NAV_LIST_INFO.map(({ icon, href, text }, index) => (
-          <li key={index} className="w-max">
-            <label className="flex-col justify-center align-middle">
-              <IconAnchor icon={icon} href={href} className="flex justify-center align-middle h-[25px]" />
-              <span className="text-overline w-max">{text}</span>
-            </label>
+          <li key={index} className="w-max justify-self-center">
+            <div className="flex flex-col justify-center items-center p-8">
+              <IconAnchor icon={icon} href={href} className="flex justify-center" />
+              <span className="text-overline w-fit text-gray-400 pt-[3.5px]">{text}</span>
+            </div>
           </li>
         ))}
       </ul>
