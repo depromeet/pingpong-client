174c77bfa0a731b51a17a65bb490fe1ab5bb9d86
L2HYUNN <donghyuni98@gmail.com>
2022-11-28 00:20:55 +0900

    :sparkles: feat(story): Add HeaderTitle Story

---
+++ src/components/common/HeaderTitle.stories.tsx
@@ -0,0 +1,15 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import HeaderTitle from './HeaderTitle';
+
+export default {
+  title: 'HeaderTitle',
+  component: HeaderTitle,
+} as ComponentMeta<typeof HeaderTitle>;
+
+const Template: ComponentStory<typeof HeaderTitle> = (args) => <HeaderTitle {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  texts: ['Ping-Pong !', '재능을 공유 할 방식을 선택해주세요'],
+};
