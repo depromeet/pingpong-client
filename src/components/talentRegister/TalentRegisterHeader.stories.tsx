6f7bc321226a1cfc249df0d10d72551be230ae29
L2HYUNN <donghyuni98@gmail.com>
2022-11-29 01:33:56 +0900

    :sparkles: feat(story): Add TalentRegisterHeader Story

---
+++ src/components/talentRegister/TalentRegisterHeader.stories.tsx
@@ -0,0 +1,20 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import TalentRegisterHeader from './TalentRegisterHeader';
+
+export default {
+  title: 'Components/TalentRegisterHeader',
+  component: TalentRegisterHeader,
+} as ComponentMeta<typeof TalentRegisterHeader>;
+
+const Template: ComponentStory<typeof TalentRegisterHeader> = (args) => <TalentRegisterHeader {...args} />;
+
+export const Share = Template.bind({});
+Share.args = {
+  sort: 'share',
+};
+
+export const Exchange = Template.bind({});
+Exchange.args = {
+  sort: 'exchange',
+};
