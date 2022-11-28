a65c72da32d4810aa2fb6ebdc88ae0d47b2d40fa
L2HYUNN <donghyuni98@gmail.com>
2022-11-28 15:57:38 +0900

    :recycle: refactor(navigationbar): Add ClassName Props

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -2,19 +2,23 @@ import { ProfileIcon, TalentRegisterIcon, TalentSearchIcon } from '@/components/
 
 import IconAnchor from './IconAnchor';
 
+interface NavigationBarProps {
+  className?: string;
+}
+
 const NAV_LIST_INFO = [
   { icon: <TalentSearchIcon />, href: '/talent/search', text: '재능 탐색' },
   { icon: <TalentRegisterIcon />, href: '/talent/register', text: '재능 등록' },
   { icon: <ProfileIcon />, href: '/profile', text: '프로필' },
 ];
 
-const NavigationBar = () => {
+const NavigationBar = ({ className }: NavigationBarProps) => {
   return (
-    <nav className="bg-primary-dark w-full h-[82px]">
+    <nav className={`bg-primary-dark w-full h-[82px] ${className}`}>
       <ul className="grid grid-cols-3">
         {NAV_LIST_INFO.map(({ icon, href, text }, index) => (
           <li key={index} className="w-max justify-self-center">
-            <div className="flex flex-col justify-center items-center p-8">
+            <div className="flex flex-col justify-center items-center pt-[8px]">
               <IconAnchor icon={icon} href={href} className="flex justify-center" />
               <span className="text-overline w-fit text-gray-400 pt-[3.5px]">{text}</span>
             </div>
