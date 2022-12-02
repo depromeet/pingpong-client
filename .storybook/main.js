c78659003a445c606aba21f9a9dd2c82fb618d30
L2HYUNN <donghyuni98@gmail.com>
2022-12-02 20:47:34 +0900

    :package: chore(storybook): Add storybook-addon-recoil-flow
    storybook에서 recoil 사용을 위한 addon 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-recoil-flow',
     {
       name: '@storybook/addon-postcss',
       options: {
