18344d7d952415a3dd4ad99fbd39742c7552ede1
L2HYUNN <donghyuni98@gmail.com>
2022-12-02 19:42:36 +0900

    :sparkles: feat: Add talentRegisterInput component
    talentRegisterInput 컴포넌트를 추가합니다.

---
+++ src/components/talentRegister/TalentRegisterInput.tsx
@@ -0,0 +1,50 @@
+import React, { memo } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { talentRegisterInputSelectorFamily } from '@/store/components/selectors';
+
+import Input from '../common/Input';
+
+export interface TalentRegisterInputOptionProps {
+  key: string;
+  title?: string;
+  explanation?: string;
+  placeholder?: string;
+  htmlFor?: string;
+  showCount?: boolean;
+  maxLength?: number;
+  error?: string;
+}
+
+interface TalentRegisterInputProps {
+  option: TalentRegisterInputOptionProps;
+}
+
+const TalentRegisterInput = ({
+  option: { key, title, explanation, placeholder, htmlFor, showCount, maxLength, error },
+}: TalentRegisterInputProps) => {
+  const [input, setInput] = useRecoilState(talentRegisterInputSelectorFamily(key));
+
+  return (
+    <>
+      {title && (
+        <label htmlFor={htmlFor} className="text-t3">
+          {title}
+        </label>
+      )}
+      {explanation && <span className="block text-b4 text-gray-400 pt-[2px]">{explanation}</span>}
+      <Input
+        id={htmlFor}
+        placeholder={placeholder}
+        showCount={showCount}
+        maxLength={maxLength}
+        value={input.contents}
+        onChange={(value) => setInput((prev) => ({ ...prev, contents: value }))}
+        error={maxLength && input.contents.length >= maxLength ? error : undefined}
+        className="mt-[8px]"
+      />
+    </>
+  );
+};
+
+export default memo(TalentRegisterInput);
