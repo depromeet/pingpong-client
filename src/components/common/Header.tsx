0a327b9d67a79ef094bab50fc6784db8784f2563
Hwang yena <center7332@naver.com>
2022-12-06 22:54:26 +0900

    :bug: fix: header to be top

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import { ArrowIcon } from '../icons';
 
 const Header = ({ title, activeButton }: Header) => {
   return (
-    <header className={`flex gap-7 items-center justify-between w-full px-7 sticky top-0 py-[5%] bg-bg-gray`}>
+    <header className={`flex gap-7 items-center justify-between w-full px-7 sticky top-0 py-[5%] bg-bg-gray z-50`}>
       <button className="w-[20px] h-[20px]">
         <ArrowIcon color="black" direction="right" className="w-[9px] h-[18px]" />
       </button>
