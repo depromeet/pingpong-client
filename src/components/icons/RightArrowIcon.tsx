5df5722e92fca3f857e4e3c22f06b5610c480579
L2HYUNN <donghyuni98@gmail.com>
2022-12-07 15:50:24 +0900

    :sparkles: feat(icon): Add RightArrowIcon

---
+++ src/components/icons/RightArrowIcon.tsx
@@ -0,0 +1,10 @@
+import type { InternalSvgProps } from '../common/Svg';
+import Svg from '../common/Svg';
+
+export const RightArrowIcon = ({ ...props }: Partial<InternalSvgProps>) => {
+  return (
+    <Svg viewBox={[0, 0, 9, 14]} width={9} height={14} color={'none'} {...props}>
+      <path d="M1.08333 0.583332L7.5 7L1.08333 13.4167" stroke="#CFCFCF" stroke-width="1.6" stroke-linejoin="round" />;
+    </Svg>
+  );
+};
