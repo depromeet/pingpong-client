ffa6eb3c26d40993863abe2b71867ee92efa4689
L2HYUNN <donghyuni98@gmail.com>
2022-12-08 15:27:57 +0900

    :recycle: refactor: Change talent register first page name

--- src/pages/talent/register/index.tsx
+++ src/pages/talent/register/index.tsx
@@ -4,15 +4,15 @@ import TalentRegisterCard from '@/components/talentRegister/TalentRegisterCard';
 
 const TITLE = ['Ping-Pong !', '재능을 공유 할 방식을 선택해주세요'];
 
-const Register = () => {
+const TalentRegister = () => {
   return (
     <div className="relative w-full h-[812px] px-[16px]">
       <HeaderTitle texts={TITLE} className="pt-[60px]" />
-      <TalentRegisterCard sort="share" className="mt-[48px]" />
-      <TalentRegisterCard sort="exchange" className="mt-[16.5px]" />
+      <TalentRegisterCard sort="SHARE" className="mt-[48px]" />
+      <TalentRegisterCard sort="EXCHANGE" className="mt-[16.5px]" />
       <NavigationBar className="fixed bottom-0 left-0" />
     </div>
   );
 };
 
-export default Register;
+export default TalentRegister;
