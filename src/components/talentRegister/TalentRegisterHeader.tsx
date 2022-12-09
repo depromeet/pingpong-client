1f87fdcb86af37097de1c9919376ed7f14982ec9
L2HYUNN <donghyuni98@gmail.com>
2022-12-09 18:17:13 +0900

    :recycle: refactor: Apply new image TalentRegisterHeader

--- src/components/talentRegister/TalentRegisterHeader.tsx
+++ src/components/talentRegister/TalentRegisterHeader.tsx
@@ -1,27 +1,30 @@
+import Image from 'next/image';
+
 import type { TalentRegisterProps } from '@/constants/talentRegister/talentRegisterType';
 
 import HeaderTitle from '../common/HeaderTitle';
 import IconAnchor from '../common/IconAnchor';
-import TalentRegisterHeaderIcon from '../icons/TalentRegisterHeaderIcon';
 import { XIcon } from '../icons/XIcon';
 
 const SHARE = {
-  color: 'bg-primary-red',
+  src: '/images/talentShare.png',
+  alt: 'talentShare',
   contents: ['나누고 싶은 재능에 대해', '알려주세요'],
 };
 
 const EXCHANGE = {
-  color: 'bg-primary-blue',
+  src: '/images/talentExchange.png',
+  alt: 'talentExchange',
   contents: ['주고 싶은 재능에 대해', '알려주세요'],
 };
 
 const TalentRegisterHeader = ({ sort, className }: TalentRegisterProps) => {
-  const { color, contents } = sort === 'SHARE' ? SHARE : EXCHANGE;
+  const { src, alt, contents } = sort === 'SHARE' ? SHARE : EXCHANGE;
 
   return (
-    <div className={`relative w-full h-[213px] ${color} ${className}`}>
+    <div className={`relative w-full ${className}`}>
       <IconAnchor icon={<XIcon className="absolute right-[16px] top-[60px]" />} href="/talent/register" />
-      <TalentRegisterHeaderIcon className="w-full" />
+      <Image src={src} alt={alt} width={375} height={187} className="w-full z-99" priority />
       <HeaderTitle texts={contents} textClassName="text-gray-100" className="absolute left-[16px] bottom-[15.5px]" />
     </div>
   );
