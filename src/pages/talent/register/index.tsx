914c6daf6cbcc6212f2fb929bf690e4457db3e34
L2HYUNN <donghyuni98@gmail.com>
2022-12-12 17:43:44 +0900

    :zap: feat: Add initializing order to TalentRegister

--- src/pages/talent/register/index.tsx
+++ src/pages/talent/register/index.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+
 import HeaderTitle from '@/components/common/HeaderTitle';
 import NavigationBar from '@/components/common/NavigationBar';
 import TalentRegisterCard from '@/components/talentRegister/TalentRegisterCard';
+import { talentRegisterOrderAtom } from '@/store/components';
 
 const TITLE = ['Ping-Pong !', '재능을 공유 할 방식을 선택해주세요'];
 
 const TalentRegister = () => {
+  const setOrder = useSetRecoilState(talentRegisterOrderAtom);
+
+  useEffect(() => {
+    setOrder(1);
+  }, [setOrder]);
+
   return (
     <div className="relative w-full h-[812px] px-[16px]">
       <HeaderTitle texts={TITLE} className="pt-[60px]" />
