17e951f647cdec437411f7b5c1f0ac51f650a9e0
Hwang yena <center7332@naver.com>
2022-12-14 01:16:40 +0900

    :sparkles: feat: login pub

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,7 +2,8 @@ import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import axios from 'axios';
 import { formatQueryString } from 'src/lib/utils';
 
-axios.defaults.baseURL = 'https://zoo-animal-api.herokuapp.com';
+//FIXME: to proxy
+axios.defaults.baseURL = 'https://www.pingpongg.shop:8080/api/v1';
 
 const handleError = (_: AxiosError) => {
   //TODO: axios error handling
@@ -12,7 +13,14 @@ const handleError = (_: AxiosError) => {
 // only for QUERY
 export const queryFetcher = async (url: string, queries?: Record<string, unknown>) => {
   const newUrl = formatQueryString(url, queries);
-  const config: AxiosRequestConfig = { method: 'GET', url: newUrl };
+  const config: AxiosRequestConfig = {
+    method: 'GET',
+    url: newUrl,
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLstZzsmIHqtowiLCJtZW1iZXJJZCI6NSwiZXhwIjoxNjcwNDIyMjEwfQ.VekC3NKcgpQMGVIB6wO6kyTNRRHOESBV1nbfdEih2Dp009EM3OMm8xi-DdpUcAm3W7z8HlGrwA67DSE8t0k28Q',
+    },
+  };
 
   try {
     const res = await axios(config);
