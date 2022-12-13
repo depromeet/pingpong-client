b82892d5bcba93a90b0eb01bcac58f59e87674e2
Hwang yena <center7332@naver.com>
2022-12-14 01:17:27 +0900

    :bug: fix: remove storybook file in pages folder

--- src/components/pages/TalentRegisterShare.stories.tsx
+++ src/components/pages/TalentRegisterShare.stories.tsx
@@ -1,6 +1,6 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import TalentRegisterShare from './index';
+import TalentRegisterShare from '../../pages/talent/register/share/index';
 
 export default {
   title: 'Pages/Talent/TalentRegisterShare',
