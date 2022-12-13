45f4a20e75f859d48597a105e1fd049dc3db57f2
L2HYUNN <donghyuni98@gmail.com>
2022-12-13 22:46:13 +0900

    :sparkles: feat: Add TalentRegisterCategory Pages

---
+++ src/pages/talent/register/share/category/index.tsx
@@ -0,0 +1,10 @@
+import TalentRegisterCategotyHeader from '@/components/talentRegister/TalentRegisterCategoryHeader';
+
+// 중복되는 코드로 추후 리팩토링 필요
+const sort = 'SHARE';
+
+const TalentRegisterExchangeCategory = () => {
+  return <TalentRegisterCategotyHeader sort={sort} />;
+};
+
+export default TalentRegisterExchangeCategory;
