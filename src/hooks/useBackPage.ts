e478b145c3b793f47cee473ef882a338472644d4
L2HYUNN <donghyuni98@gmail.com>
2022-12-13 21:23:13 +0900

    :recycle: refactor: Add order protect logic to PageHooks

--- src/hooks/useBackPage.ts
+++ src/hooks/useBackPage.ts
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import type { RecoilState } from 'recoil';
 import { useRecoilState } from 'recoil';
 
+const ORDER_LIMIT = 1;
+
 const useBackPage = (atom: RecoilState<number>) => {
   const [order, setOrder] = useRecoilState(atom);
+
+  useEffect(() => {
+    order < ORDER_LIMIT && setOrder(ORDER_LIMIT);
+  }, [order, setOrder]);
 
   const handleOrder = () => {
     setOrder((prev) => prev - 1);
