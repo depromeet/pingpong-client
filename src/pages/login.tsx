9bc8eba50dab8e63f945ddb6c895967ffbb2dcb8
Hwang yena <center7332@naver.com>
2022-12-14 01:18:29 +0900

    :sparkles: feat: login fetch

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,17 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import React from 'react';
 
-import Button from '@/components/common/Button';
+import Button from '../components/common/Button';
 
 export default function Login() {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    //FIXME: maybe change server url
+    router.push(process.env.NEXT_PUBLIC_REDIRECT_URL ?? '');
+  };
+
   return (
     <main className="w-screen h-screen bg-primary-blue p-[16px] flex flex-col items-center">
       <section className="flex flex-col items-center gap-[16px] mt-[50%]">
@@ -10,7 +19,7 @@ export default function Login() {
         <p className="text-white text-t3">재능을 교환하고 나누며 성장하는 플랫폼</p>
       </section>
       <section className="w-full flex flex-col h-full justify-end">
-        <Button onClick={() => null} className="bg-black w-full">
+        <Button onClick={handleLogin} className="bg-black w-full">
           <div className="flex text-white items-center justify-center gap-[12px]">
             <Image src={'/icons/apple.svg'} alt="apple" width={15} height={15} />
             Apple로 시작하기
