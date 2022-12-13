91a2254c08f85ff24aaaa4ca59daea91c2e9fc92
L2HYUNN <donghyuni98@gmail.com>
2022-12-13 18:26:07 +0900

    :bug: fix: Add NextImage unoptimized option storybook
    NextImage가 나오지 않는 오류 해결을 위한 unoptimized 옵션 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,15 @@
 import '../src/styles/globals.css';
 
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+
+// eslint-disable-next-line no-import-assign
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
