2e6b15c47dcf7b847b7223a55b15973365784345
L2HYUNN <donghyuni98@gmail.com>
2022-12-14 22:46:12 +0900

    :sparkles: feat: Add categoryKey prop

--- src/components/common/TabList.tsx
+++ src/components/common/TabList.tsx
@@ -3,14 +3,19 @@ import type { TabProps } from 'src/store/components/types';
 import Tab from './Tab';
 
 interface TabListProps {
+  categoryKey: string;
   list: TabProps[];
 }
 
-const TabList = ({ list }: TabListProps) => {
+const TabList = ({ categoryKey, list }: TabListProps) => {
   return (
-    <ul>
-      {list.map(({ id, content }) => {
-        return <li key={id}>{<Tab id={id} content={content} />}</li>;
+    <ul className="flex min-w-[375px] overflow-x-scroll">
+      {list.map(({ id, name }) => {
+        return (
+          <li key={id} className="px-[8px] border-b-[1px] w-fit">
+            {<Tab categoryKey={categoryKey} id={id} name={name} />}
+          </li>
+        );
       })}
     </ul>
   );
