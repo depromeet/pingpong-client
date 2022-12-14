6e611669e409ce1cdd150a259814ee4152ffb226
L2HYUNN <donghyuni98@gmail.com>
2022-12-14 22:50:16 +0900

    :sparkles: feat: Add TabList to Category pages

--- src/pages/talent/register/exchange/category/index.tsx
+++ src/pages/talent/register/exchange/category/index.tsx
@@ -1,10 +1,16 @@
 import TalentRegisterCategotyHeader from '@/components/talentRegister/TalentRegisterCategoryHeader';
+import TalentRegisterCategoryTabList from '@/components/talentRegister/TalentRegisterCategoryTabList';
 
 // 중복되는 코드로 추후 리팩토링 필요
 const sort = 'EXCHANGE';
 
 const TalentRegisterExchangeCategory = () => {
-  return <TalentRegisterCategotyHeader sort={sort} />;
+  return (
+    <>
+      <TalentRegisterCategotyHeader sort={sort} />;
+      <TalentRegisterCategoryTabList />
+    </>
+  );
 };
 
 export default TalentRegisterExchangeCategory;
