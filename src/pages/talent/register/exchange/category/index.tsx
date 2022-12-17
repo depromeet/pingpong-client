0330ebd3a95f515d2b6aff7e173e26d388ceb560
L2HYUNN <donghyuni98@gmail.com>
2022-12-17 13:28:56 +0900

    :sparkles: feat: Add TalentRegisterCategoryTagList to pages

--- src/pages/talent/register/exchange/category/index.tsx
+++ src/pages/talent/register/exchange/category/index.tsx
@@ -1,5 +1,6 @@
 import TalentRegisterCategotyHeader from '@/components/talentRegister/TalentRegisterCategoryHeader';
 import TalentRegisterCategoryTabList from '@/components/talentRegister/TalentRegisterCategoryTabList';
+import TalentRegisterCategoryTagList from '@/components/talentRegister/TalentRegisterCategoryTagList';
 
 // 중복되는 코드로 추후 리팩토링 필요
 const sort = 'EXCHANGE';
@@ -7,8 +8,9 @@ const sort = 'EXCHANGE';
 const TalentRegisterExchangeCategory = () => {
   return (
     <>
-      <TalentRegisterCategotyHeader sort={sort} />;
+      <TalentRegisterCategotyHeader sort={sort} />
       <TalentRegisterCategoryTabList />
+      <TalentRegisterCategoryTagList sort={sort} />
     </>
   );
 };
--- src/pages/talent/register/share/category/index.tsx
+++ src/pages/talent/register/share/category/index.tsx
@@ -1,5 +1,6 @@
 import TalentRegisterCategotyHeader from '@/components/talentRegister/TalentRegisterCategoryHeader';
 import TalentRegisterCategoryTabList from '@/components/talentRegister/TalentRegisterCategoryTabList';
+import TalentRegisterCategoryTagList from '@/components/talentRegister/TalentRegisterCategoryTagList';
 
 // 중복되는 코드로 추후 리팩토링 필요
 const sort = 'SHARE';
@@ -9,6 +10,7 @@ const TalentRegisterShareCategory = () => {
     <>
       <TalentRegisterCategotyHeader sort={sort} />
       <TalentRegisterCategoryTabList />
+      <TalentRegisterCategoryTagList sort={sort} />
     </>
   );
 };
