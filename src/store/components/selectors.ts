84b000de495dbe1a953ff874d09eb3aded8c1829
L2HYUNN <donghyuni98@gmail.com>
2022-12-17 13:13:59 +0900

    :sparkles: feat: Add talentRegister selector logics
    재능 등록에 필요한 selector 로직 추가

--- src/store/components/selectors.ts
+++ src/store/components/selectors.ts
@@ -1,10 +1,17 @@
-import { selector, selectorFamily } from 'recoil';
+import { DefaultValue, selector, selectorFamily } from 'recoil';
 
-import { talentRegisterInputAtomFamily, talentRegisterInputKeyAtom } from './atoms';
-import type { TalentRegisterInputInfo } from './types';
+import { makeAndRemoveAtomKey } from '@/lib/utils';
+
+import {
+  tabAtomFamily,
+  talentRegisterInputAtomFamily,
+  talentRegisterInputKeyAtom,
+  talentRegisterInputLinkKeyAtom,
+  talentRegisterMethodAtom,
+} from './atoms';
+import type { TalentRegisterInfoProps, TalentRegisterInputInfo } from './types';
 
 // INFO:: selectorFamily를 통해 Key 관리 작업을 recoil 내에서 처리할 수 있습니다.
-// TODO:: input 데이터가 ''일시 keyAtom에서 제거하는 작업이 필요합니다.
 const talentRegisterInputSelectorFamily = selectorFamily<TalentRegisterInputInfo, string>({
   key: 'talentRegisterInputSelector',
   get:
@@ -16,7 +23,13 @@ const talentRegisterInputSelectorFamily = selectorFamily<TalentRegisterInputInfo
     ({ get, set }, newInput) => {
       const input = get(talentRegisterInputAtomFamily(inputKey));
 
-      set(talentRegisterInputKeyAtom, (prev) => Array.from(new Set([...prev, input.inputKey])));
+      if (!(newInput instanceof DefaultValue)) {
+        if (input.inputKey.includes('link')) {
+          set(talentRegisterInputLinkKeyAtom, (prev) => makeAndRemoveAtomKey({ prev, newInput, input }));
+        } else {
+          set(talentRegisterInputKeyAtom, (prev) => makeAndRemoveAtomKey({ prev, newInput, input }));
+        }
+      }
       set(talentRegisterInputAtomFamily(inputKey), newInput);
     },
 });
@@ -24,12 +37,8 @@ const talentRegisterInputSelectorFamily = selectorFamily<TalentRegisterInputInfo
 const talentRegisterLinksSelector = selector({
   key: 'talentRegisterLinksSelector',
   get: ({ get }) => {
-    const keyList = get(talentRegisterInputKeyAtom);
-
-    const linkKeyList = keyList.filter((key) => {
-      return key.includes('link');
-    });
-    const linkList = linkKeyList.map((key) => {
+    const keyList = get(talentRegisterInputLinkKeyAtom);
+    const linkList = keyList.map((key) => {
       const link = get(talentRegisterInputAtomFamily(key));
       return link.contents;
     });
@@ -38,4 +47,42 @@ const talentRegisterLinksSelector = selector({
   },
 });
 
-export { talentRegisterInputSelectorFamily, talentRegisterLinksSelector };
+const talentRegisterTakenCategoriesSelector = selector({
+  key: 'talentRegisterTakenCategories',
+  get: ({ get }) => {
+    const takenTalentCategories = get(tabAtomFamily('takenTalentIds'));
+    const takentTalentIds = takenTalentCategories.map((category) => category.id);
+
+    return takentTalentIds;
+  },
+});
+
+const talentRegisterSelector = selector({
+  key: 'talentRegisterSelector',
+  get: ({ get }) => {
+    const talentRegisterInfo: TalentRegisterInfoProps = {};
+
+    const isShare = get(talentRegisterMethodAtom);
+    talentRegisterInfo['isShare'] = isShare;
+
+    // TODO: 추후 동적 Key 관리가 필요합니다.
+    const subCategory = get(tabAtomFamily('subCategoryId'));
+    if (subCategory?.length > 0) talentRegisterInfo['subCategoryId'] = subCategory[0].id;
+
+    const inputKeys = get(talentRegisterInputKeyAtom);
+    inputKeys.forEach((inputKey) => {
+      const input = get(talentRegisterInputAtomFamily(inputKey));
+      talentRegisterInfo[input.inputKey] = input.contents;
+    });
+
+    const links = get(talentRegisterLinksSelector);
+    if (links.length > 0) talentRegisterInfo['links'] = links;
+
+    const takenTalentIds = get(talentRegisterTakenCategoriesSelector);
+    if (takenTalentIds?.length > 0) talentRegisterInfo['takenTalentIds'] = takenTalentIds;
+
+    return talentRegisterInfo;
+  },
+});
+
+export { talentRegisterInputSelectorFamily, talentRegisterLinksSelector, talentRegisterSelector };
