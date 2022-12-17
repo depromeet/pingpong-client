aee89d1864ffc4130592df6607152b8c07380093
L2HYUNN <donghyuni98@gmail.com>
2022-12-17 13:07:12 +0900

    :sparkles: feat: Add MakeAndRemoveAtomKeyProps type
    util에 있는 key 함수와 연결되는 타입입니다.

---
+++ src/lib/types.ts
@@ -0,0 +1,7 @@
+import type { TalentRegisterInputInfo } from '@/store/components/types';
+
+export interface MakeAndRemoveAtomKeyProps {
+  prev: string[];
+  newInput: TalentRegisterInputInfo;
+  input: TalentRegisterInputInfo;
+}
