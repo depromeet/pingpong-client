e04c98432cf154ec0b0e13a6e87a246fd5f0be2e
doyoonear <doyoonleee@gmail.com>
2022-12-20 02:53:28 +0900

    :sparkles: feat: Add post like mutation

--- src/components/common/LikeButton.tsx
+++ src/components/common/LikeButton.tsx
@@ -7,11 +7,18 @@ import IconLikeFilled from '../../../public/icons/like-filled.svg';
 
 type LikeButtonStyle = 'VERTICAL' | 'HORIZONTAL';
 
-const LikeButton = ({ type, isFilled, count }: { type: LikeButtonStyle; isFilled: boolean; count: string }) => {
+interface LikeButtonProps {
+  type: LikeButtonStyle;
+  isFilled: boolean;
+  count: string;
+  onClick: () => void;
+}
+
+const LikeButton = ({ type, isFilled, count, onClick }: LikeButtonProps) => {
   return (
-    <LikeButtonContainer type={type}>
+    <LikeButtonContainer type={type} onClick={onClick}>
       <IconWrapper type={type}>{isFilled ? <IconLikeFilled /> : <IconLikeEmpty />}</IconWrapper>
-      <LikeCount>{count}</LikeCount>
+      <LikeCount type={type}>{count}</LikeCount>
     </LikeButtonContainer>
   );
 };
@@ -21,7 +28,6 @@ export default LikeButton;
 const LikeButtonContainer = styled.div<{ type: LikeButtonStyle }>`
   display: flex;
   align-items: center;
-  color: ${colors.black};
   ${({ type }: { type: LikeButtonStyle }) => type === 'VERTICAL' && 'flex-direction: column; width: 4.8rem;'};
 `;
 
@@ -30,11 +36,12 @@ const IconWrapper = styled.div<{ type: LikeButtonStyle }>`
     type === 'VERTICAL' ? `width: 2rem; height: 2rem;` : `width: 1.4rem; height: 1.4rem;`};
 `;
 
-const LikeCount = styled.p`
+const LikeCount = styled.p<{ type: LikeButtonStyle }>`
   text-align: center;
+  color: ${colors.black};
 
   ${({ type }: { type: LikeButtonStyle }) =>
     type === 'VERTICAL'
-      ? `font-size: 1.2rem; line-height: 1.8rem; letter-spacing: -0.03rem; font-weight: 500;`
-      : `font-size: 1.2rem; line-height: 1.8rem; letter-spacing: -0.03rem; font-weight: 500; margin-left: 1.2rem;`};
+      ? `font-size: 1.2rem; line-height: 1.8rem; letter-spacing: -0.03rem; font-weight: 500; margin-left: 0;`
+      : `font-size: 1.4rem; line-height: 2.1rem; letter-spacing: -0.03rem; font-weight: 500; margin-left: 1.2rem;`};
 `;
