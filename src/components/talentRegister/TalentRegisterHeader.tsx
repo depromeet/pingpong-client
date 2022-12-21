998ca0d25b76951ebd33f89e2a28a00897d8e88a
L2HYUNN <donghyuni98@gmail.com>
2022-12-21 19:13:40 +0900

    :sparkles: feat: Add popup to talent register page
    재능 등록 페이지에 팝업을 추가합니다

--- src/components/talentRegister/TalentRegisterHeader.tsx
+++ src/components/talentRegister/TalentRegisterHeader.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
-import { useRecoilValue } from 'recoil';
+import { useRouter } from 'next/router';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import type { TalentRegisterProps } from '@/constants/talentRegister/talentRegisterType';
-import { talentRegisterOrderAtom } from '@/store/components';
+import { popupAtom, talentRegisterOrderAtom } from '@/store/components';
+import { talentRegisterResetSelector } from '@/store/components/selectors';
 
 import HeaderTitle from '../common/HeaderTitle';
-import IconAnchor from '../common/IconAnchor';
 import { XIcon } from '../icons/XIcon';
 
 const SHARE = {
@@ -22,13 +23,35 @@ const EXCHANGE = {
   contents2: ['받고 싶은 재능에 대해', '알려주세요'],
 };
 
+const POPUP_INFO = {
+  title: '게시글 작성을 그만두시겠어요?',
+  content: '지금까지 작성한 내용은 저장되지 않아요',
+  confirmText: '그만둘래요',
+  cancelText: '취소',
+};
+
 const TalentRegisterHeader = ({ sort, className }: TalentRegisterProps) => {
   const { src, alt, contents, contents2 } = sort === 'SHARE' ? SHARE : EXCHANGE;
   const order = useRecoilValue(talentRegisterOrderAtom);
+  const setPopup = useSetRecoilState(popupAtom);
+  const setReset = useSetRecoilState(talentRegisterResetSelector);
+  const router = useRouter();
+
+  const handleClick = () => {
+    setPopup({
+      ...POPUP_INFO,
+      onConfirm: () => {
+        router.push('/talent/register');
+        setReset(true);
+      },
+    });
+  };
 
   return (
     <div className={`relative w-full ${className}`}>
-      <IconAnchor icon={<XIcon className="absolute right-[16px] top-[60px]" />} href="/talent/register" />
+      <button type="button" onClick={handleClick}>
+        <XIcon className="absolute right-[16px] top-[60px]" />
+      </button>
       <Image src={src} alt={alt} width={375} height={187} className="w-full z-99" priority />
       <HeaderTitle
         texts={sort === 'EXCHANGE' && (order === 2 || order == 3) ? contents2 : contents}
