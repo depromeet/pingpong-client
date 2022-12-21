4c303f03e3777775eda63b223980c29d715e98c2
L2HYUNN <donghyuni98@gmail.com>
2022-12-21 17:23:48 +0900

    :recycle: refactor: Refactor Popup component styles

--- src/components/common/Popup.tsx
+++ src/components/common/Popup.tsx
@@ -23,26 +23,25 @@ const Popup = ({ children, content, title, cancelText, confirmText, onCancel, on
 
   return (
     <Portal isShowing className="absolute w-screen h-screen left-0 top-0 grid place-items-center px-[6%]">
-      <div className="rounded-[16px] w-full bg-white z-10">
+      <div className="rounded-[16px] w-full bg-white z-50">
         <main className="px-5 py-[8%] flex items-center gap-2 flex-col">
-          <h1 className="text-[16px] text-center text-gray-900 font-semibold">{title}</h1>
-          {content && (
-            <p className="text-gray-500 text-center text-[15px]" dangerouslySetInnerHTML={{ __html: content }} />
-          )}
+          <h1 className="text-t2 text-center text-gray-600">{title}</h1>
+          {content && <p className="text-gray-400 text-center text-b2" dangerouslySetInnerHTML={{ __html: content }} />}
           {children}
         </main>
         <footer className="flex w-full text-[15px] items-center justify-center border-t-[1px] border-gray-150">
-          {onCancel && (
-            <button className=" w-full py-[4%] border-r-[1px] border-gray-150 text-gray-550" onClick={onCancelClicked}>
-              {cancelText ?? '취소'}
-            </button>
-          )}
-          <button className="text-blue-300 w-full py-[4%] font-medium" onClick={onConfirmClicked}>
+          <button
+            className="text-button w-full py-[4%] border-r-[1px] border-gray-150 text-gray-400"
+            onClick={onCancelClicked}
+          >
+            {cancelText ?? '취소'}
+          </button>
+          <button className="text-primary-dark w-full py-[4%] text-button" onClick={onConfirmClicked}>
             {confirmText ?? '확인'}
           </button>
         </footer>
       </div>
-      <div className="fixed w-screen h-screen left-0 top-0 bg-black opacity-50" />
+      <div className="fixed w-screen h-screen left-0 top-0 bg-black opacity-50 z-40" />
     </Portal>
   );
 };
