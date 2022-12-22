e3d90f22c4c900a72bbeb82059455fc9e120e707
doyoonear <doyoonleee@gmail.com>
2022-12-22 23:52:09 +0900

    :sparkles: feat: Add bottom fixed bar

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import type { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 
 import { ButtonStyleType } from '../../constants/components';
@@ -8,49 +9,29 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   iconName?: string;
   position?: string;
   disabled?: boolean;
-  onClick: () => void;
+  onClick?: () => void;
   className?: string;
 }
 
 const Button = ({
   buttonStyle = ButtonStyleType.PRIMARY,
   type = 'button',
-  // hasIcon = false,
-  // iconName = 'arrow-right',
-  position = 'right',
   children,
   disabled = false,
   onClick,
   className,
 }: PropsWithChildren<ButtonProps>) => {
-  // TODO: storybook 내에서 svg import 가 안되는 이슈 발생하여 해결중
-  // const DynamicIcon = dynamic(() => import(`../../../public/icons/${iconName}.svg`));
-  const defaultClass = 'text-button px-24 py-16 rounded-xl';
+  const defaultClass = 'text-button w-full px-24 py-16 h-48 rounded-xl';
 
   return (
-    <div className="flex w-full">
-      {position === 'right' ? (
-        <button
-          type={type}
-          className={`btn-${buttonStyle.toLowerCase()} ${defaultClass} ${className}`}
-          onClick={onClick}
-          disabled={disabled}
-        >
-          {children}
-          {/* {hasIcon && <DynamicIcon />} */}
-        </button>
-      ) : (
-        <button
-          type={type}
-          className={`btn-${buttonStyle.toLowerCase()} ${defaultClass} ${className}`}
-          onClick={onClick}
-          disabled={disabled}
-        >
-          {/* {hasIcon && <DynamicIcon />} */}
-          {children}
-        </button>
-      )}
-    </div>
+    <button
+      type={type}
+      className={`btn-${buttonStyle.toLowerCase()} ${defaultClass} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
   );
 };
 
