f275ef63aa401773f3db846647c84b22e719e189
L2HYUNN <donghyuni98@gmail.com>
2022-12-22 20:13:04 +0900

    :bug: fix: Change TextSelectInput input props
    SelectInput input props 변경에 따른 스토리 props 변경

--- src/components/common/TextSelectInput.stories.tsx
+++ src/components/common/TextSelectInput.stories.tsx
@@ -1,5 +1,6 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
+import SelectInput from './SelectInput';
 import TextSelectInput from './TextSelectInput';
 
 export default {
@@ -7,18 +8,70 @@ export default {
   component: TextSelectInput,
 } as ComponentMeta<typeof TextSelectInput>;
 
-const Template: ComponentStory<typeof TextSelectInput> = (args) => <TextSelectInput {...args} />;
+const Template: ComponentStory<typeof TextSelectInput> = ({ option }) => {
+  const input =
+    option.key === 'default'
+      ? []
+      : [
+          { id: 0, name: '액세서리/패션소품' },
+          { id: 1, name: '업무 자동화' },
+          { id: 2, name: '프론트엔드' },
+          { id: 3, name: '백엔드' },
+        ];
+
+  return (
+    <div className={option.className}>
+      {option.title && (
+        <div className="relative">
+          <label htmlFor={option.htmlFor} className="text-t3">
+            {option.title}
+          </label>
+          {option.required && <span className="absolute text-primary-red text-b4 ml-[3px] top-[0px]">*</span>}
+        </div>
+      )}
+      {option.explanation && <span className="block text-b4 text-gray-400 pt-[2px]">{option.explanation}</span>}
+      <SelectInput placeholder={option.placeholder} href={option.href} selectedInputList={input} className="mt-[8px]" />
+    </div>
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {
   option: {
-    key: 'category',
-    href: 'talent/register/category',
+    key: 'default',
+    href: '',
+    title: '어떤 재능을 나누고 싶나요?',
+    explanation: '',
+    placeholder: '카테고리를 선택해 주세요.',
+    htmlFor: 'category',
+    required: true,
+    className: 'mb-[28px]',
+  },
+};
+
+export const Share = Template.bind({});
+Share.args = {
+  option: {
+    key: 'subCategoryShare',
+    href: '',
+    title: '어떤 재능을 나누고 싶나요?',
+    explanation: '',
+    placeholder: '카테고리를 선택해 주세요.',
+    htmlFor: 'category',
+    required: true,
+    className: 'mb-[28px]',
+  },
+};
+
+export const Exchange = Template.bind({});
+Exchange.args = {
+  option: {
+    key: 'subCategoryExchange',
+    href: '',
     title: '어떤 재능을 주고 싶나요?',
     explanation: '',
     placeholder: '카테고리를 선택해 주세요.',
     htmlFor: 'category',
-    selectedInputList: ['영어'],
     required: true,
     className: 'mb-[28px]',
   },
