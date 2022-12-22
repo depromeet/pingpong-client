6630e6ddd800db86ad279f673f75e528f7354a7b
L2HYUNN <donghyuni98@gmail.com>
2022-12-22 20:13:04 +0900

    :bug: fix: Change TextSelectInput input props
    SelectInput input props 변경에 따른 스토리 props 변경

--- src/components/common/SelectInput.stories.tsx
+++ src/components/common/SelectInput.stories.tsx
@@ -14,10 +14,16 @@ const Template: ComponentStory<typeof SelectInput> = (args) => (
 export const Default = Template.bind({});
 Default.args = {
   placeholder: '카테고리를 선택해 주세요.',
+  selectedInputList: [],
 };
 
 export const Value = Template.bind({});
 Value.args = {
   placeholder: '카테고리를 선택해 주세요.',
-  selectedInputList: ['액세서리/패션소품', '업무 자동화', '프론트엔드', '제2의 장벽'],
+  selectedInputList: [
+    { id: 0, name: '액세서리/패션소품' },
+    { id: 1, name: '업무 자동화' },
+    { id: 2, name: '프론트엔드' },
+    { id: 3, name: '백엔드' },
+  ],
 };
