35a5313fb9c0ca44a9e19e3cede283ce8414813c
doyoonear <doyoonleee@gmail.com>
2022-12-23 00:40:57 +0900

    :bug: fix: Fix setState function typo

--- src/components/posts/PostHeaderNav.tsx
+++ src/components/posts/PostHeaderNav.tsx
@@ -1,19 +1,21 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
+
+import useBottomSheet from '@/hooks/useBottomSheet';
 
 import ArrowLeftIcon from '../../../public/icons/arrow-left.svg';
 import MoreIcon from '../../../public/icons/more.svg';
 
 const PostHeaderNav = () => {
   const router = useRouter();
+  const { setIsShowing } = useBottomSheet();
 
   return (
     <HeaderNavContainer>
       <button onClick={() => router.back()}>
         <ArrowLeftIcon />
       </button>
-      <MoreIcon />
+      <MoreIcon onClick={() => setIsShowing(true)} />
     </HeaderNavContainer>
   );
 };
