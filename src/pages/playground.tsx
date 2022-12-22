327ada36ec01a724936cf61107d6e5ac3a0db15d
L2HYUNN <donghyuni98@gmail.com>
2022-12-22 23:10:08 +0900

    :bug: fix: Fix CD error in playround.tsx

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -8,7 +8,7 @@ import CircleImg from '@/components/common/CircleImg';
 import RadioButtonGroup from '@/components/common/RadioButtonGroup';
 import RadioGroup from '@/components/common/RadioGroup';
 import useBottomSheet from '@/hooks/useBottomSheet';
-import useRadioGroup from '@/hooks/useRadioGroup';
+// import useRadioGroup from '@/hooks/useRadioGroup';
 
 const Playground = () => {
   const { isShowing, setIsShowing } = useBottomSheet();
@@ -19,88 +19,88 @@ const Playground = () => {
     { id: 2, label: '후후' },
   ];
 
-  const { list, currentSelected, onChange } = useRadioGroup([
-    {
-      key: 'INCORRECT',
-      label: '사용자 정보가 부정확해요.',
-    },
-    {
-      key: 'MARKETING',
-      label: '광고성/홍보성 글이에요.',
-    },
-    {
-      key: 'DUPLICATED',
-      label: '이 게시글이 도배 되어있어요.',
-    },
-  ]);
+  // const { list, currentSelected, onChange } = useRadioGroup([
+  //   {
+  //     key: 'INCORRECT',
+  //     label: '사용자 정보가 부정확해요.',
+  //   },
+  //   {
+  //     key: 'MARKETING',
+  //     label: '광고성/홍보성 글이에요.',
+  //   },
+  //   {
+  //     key: 'DUPLICATED',
+  //     label: '이 게시글이 도배 되어있어요.',
+  //   },
+  // ]);
 
-  const {
-    list: locationList,
-    currentSelected: selectedLocation,
-    onChange: onChangeLocation,
-  } = useRadioGroup([
-    {
-      key: 'ONLINE',
-      label: '온라인',
-    },
-    {
-      key: 'OFFLINE',
-      label: '오프라인',
-    },
-    {
-      key: 'BOTH',
-      label: '상관 없음',
-    },
-  ]);
+  // const {
+  //   list: locationList,
+  //   currentSelected: selectedLocation,
+  //   onChange: onChangeLocation,
+  // } = useRadioGroup([
+  //   {
+  //     key: 'ONLINE',
+  //     label: '온라인',
+  //   },
+  //   {
+  //     key: 'OFFLINE',
+  //     label: '오프라인',
+  //   },
+  //   {
+  //     key: 'BOTH',
+  //     label: '상관 없음',
+  //   },
+  // ]);
 
-  const {
-    list: periodList,
-    currentSelected: selectedPeriod,
-    onChange: onChangePeriod,
-  } = useRadioGroup([
-    {
-      key: 'DAYLY',
-      label: '1주 미만',
-    },
-    {
-      key: 'WEEKLY',
-      label: '1주 이상',
-    },
-    {
-      key: 'MONTHLY',
-      label: '1개월 이상',
-    },
-    {
-      key: 'NONE',
-      label: '조율 가능',
-    },
-  ]);
+  // const {
+  //   list: periodList,
+  //   currentSelected: selectedPeriod,
+  //   onChange: onChangePeriod,
+  // } = useRadioGroup([
+  //   {
+  //     key: 'DAYLY',
+  //     label: '1주 미만',
+  //   },
+  //   {
+  //     key: 'WEEKLY',
+  //     label: '1주 이상',
+  //   },
+  //   {
+  //     key: 'MONTHLY',
+  //     label: '1개월 이상',
+  //   },
+  //   {
+  //     key: 'NONE',
+  //     label: '조율 가능',
+  //   },
+  // ]);
 
-  const {
-    list: timeList,
-    currentSelected: selectedTime,
-    onChange: onChangeTime,
-  } = useRadioGroup([
-    {
-      key: 'MORNING',
-      label: '오전',
-      subLabel: '6AM - 12PM',
-    },
-    {
-      key: 'AFTERNOON',
-      label: '오후',
-      subLabel: '12PM - 6PM',
-    },
-    {
-      key: 'NIGHT',
-      label: '밤',
-      subLabel: '6PM - 12AM',
-    },
-    {
-      key: 'NONE',
-      label: '조율 가능',
-    },
-  ]);
+  // const {
+  //   list: timeList,
+  //   currentSelected: selectedTime,
+  //   onChange: onChangeTime,
+  // } = useRadioGroup([
+  //   {
+  //     key: 'MORNING',
+  //     label: '오전',
+  //     subLabel: '6AM - 12PM',
+  //   },
+  //   {
+  //     key: 'AFTERNOON',
+  //     label: '오후',
+  //     subLabel: '12PM - 6PM',
+  //   },
+  //   {
+  //     key: 'NIGHT',
+  //     label: '밤',
+  //     subLabel: '6PM - 12AM',
+  //   },
+  //   {
+  //     key: 'NONE',
+  //     label: '조율 가능',
+  //   },
+  // ]);
 
   const cardMockData = {
     id: 7,
@@ -174,23 +174,23 @@ const Playground = () => {
       </PlaygroundBlock>
       <PlaygroundBlock>
         <PlaygroundTitle>Radio (Group)</PlaygroundTitle>
-        <RadioGroup list={list} currentSelected={currentSelected} onChange={onChange} />
+        {/* <RadioGroup list={list} currentSelected={currentSelected} onChange={onChange} /> */}
       </PlaygroundBlock>
       <PlaygroundBlock>
         <PlaygroundTitle>Radio Button (Group)</PlaygroundTitle>
         <PlaygroundRow>
-          <RadioButtonGroup
+          {/* <RadioButtonGroup
             size="small"
             list={locationList}
             currentSelected={selectedLocation}
             onChange={onChangeLocation}
-          />
+          /> */}
         </PlaygroundRow>
         <PlaygroundRow>
-          <RadioButtonGroup list={periodList} currentSelected={selectedPeriod} onChange={onChangePeriod} />
+          {/* <RadioButtonGroup list={periodList} currentSelected={selectedPeriod} onChange={onChangePeriod} /> */}
         </PlaygroundRow>
         <PlaygroundRow>
-          <RadioButtonGroup list={timeList} currentSelected={selectedTime} onChange={onChangeTime} />
+          {/* <RadioButtonGroup list={timeList} currentSelected={selectedTime} onChange={onChangeTime} /> */}
         </PlaygroundRow>
       </PlaygroundBlock>
       <PlaygroundBlock>
