e0310fc2764eaf7a9f05548d38555c363b028a20
L2HYUNN <donghyuni98@gmail.com>
2022-12-23 18:03:20 +0900

    :sparkles: feat: Add GNB selected logic

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 import { ProfileIcon, TalentRegisterIcon, TalentSearchIcon } from '@/components/icons';
 
 import IconAnchor from './IconAnchor';
@@ -7,20 +10,36 @@ interface NavigationBarProps {
 }
 
 const NAV_LIST_INFO = [
-  { icon: <TalentSearchIcon />, href: '/talent/search', text: '재능 탐색' },
-  { icon: <TalentRegisterIcon />, href: '/talent/register', text: '재능 등록' },
-  { icon: <ProfileIcon />, href: '/profile', text: '프로필' },
+  { icon: <TalentSearchIcon />, selectedIcon: <TalentSearchIcon color="#4D77FF" />, href: '/', text: '재능 탐색' },
+  {
+    icon: <TalentRegisterIcon />,
+    selectedIcon: <TalentRegisterIcon color="#4D77FF" />,
+    href: '/talent/register',
+    text: '재능 등록',
+  },
+  { icon: <ProfileIcon />, selectedIcon: <ProfileIcon color="#4D77FF" />, href: '/profile', text: '프로필' },
 ];
 
 const NavigationBar = ({ className }: NavigationBarProps) => {
+  const [selected, setSelected] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    setSelected(router.asPath);
+  }, [router.asPath]);
+
   return (
-    <nav className={`bg-primary-dark w-full h-[82px] ${className}`}>
+    <nav className={`bg-white w-full h-[82px] ${className}`}>
       <ul className="grid grid-cols-3">
-        {NAV_LIST_INFO.map(({ icon, href, text }, index) => (
+        {NAV_LIST_INFO.map(({ icon, selectedIcon, href, text }, index) => (
           <li key={index} className="w-max justify-self-center">
             <div className="flex flex-col justify-center items-center pt-[8px]">
-              <IconAnchor icon={icon} href={href} className="flex justify-center" />
-              <span className="text-overline w-fit text-gray-400 pt-[3.5px]">{text}</span>
+              <IconAnchor icon={selected === href ? selectedIcon : icon} href={href} className="flex justify-center" />
+              <span
+                className={`text-overline w-fit ${selected === href ? 'text-primary-blue' : 'text-gray-400'}  pt-[3px]`}
+              >
+                {text}
+              </span>
             </div>
           </li>
         ))}
