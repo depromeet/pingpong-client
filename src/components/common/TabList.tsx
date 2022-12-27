2a83dfd2e42f77c343aa34a2b42e3d9750d0011d
doyoonear <doyoonleee@gmail.com>
2022-12-27 20:30:43 +0900

    :bug: fix: Make mainCategoryName optional

--- src/components/common/TabList.tsx
+++ src/components/common/TabList.tsx
@@ -5,15 +5,17 @@ import Tab from './Tab';
 interface TabListProps {
   categoryKey: string;
   list: TabProps[];
-  mainCategoryName: string;
+  mainCategoryName?: string;
 }
 
 const TabList = ({ categoryKey, list, mainCategoryName }: TabListProps) => {
   return (
     <ul className="flex min-w-[375px] overflow-x-scroll border-b-[1px] pl-8 mb-12">
-      <li key="ALL" className="px-[10px] w-fit">
-        {<Tab categoryKey={categoryKey} id="ALL" name={`${mainCategoryName} 전체`} />}
-      </li>
+      {mainCategoryName && (
+        <li key="ALL" className="px-[10px] w-fit">
+          {<Tab categoryKey={categoryKey} id="ALL" name={`${mainCategoryName} 전체`} />}
+        </li>
+      )}
       {list.map(({ id, name }) => {
         return (
           <li key={id} className="px-[10px] w-fit">
