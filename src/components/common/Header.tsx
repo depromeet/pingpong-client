ca54afacab900b7e7379bc62a460a2613610d331
L2HYUNN <donghyuni98@gmail.com>
2022-12-28 17:21:15 +0900

    :recycle: refactor: Add onClick to Header component
    onClick 함수 수정을 위해 외부 주입 방식으로 코드 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,19 @@
-import { useRouter } from 'next/router';
-
 import type { HeaderProps } from '@/typings/common';
 
 import { ArrowIcon } from '../icons';
 
-const Header = ({ title, activeButton, className }: HeaderProps) => {
-  const router = useRouter();
+const Header = ({ title, onClick, activeButton, className }: HeaderProps) => {
+  const handleClick = () => {
+    onClick && onClick();
+  };
 
   return (
     <header
-      className={`flex gap-7 items-center w-full px-7 sticky top-0 py-[5%] bg-bg-gray z-50 ${
+      className={`flex gap-7 items-center w-full px-7 sticky top-0 py-[5%] bg-bg-gray z-30 ${
         activeButton ? 'justify-between' : ''
       } ${className}`}
     >
-      <button className={`w-[20px] h-[20px] ${activeButton ? 'left-0' : 'absolute'}`} onClick={() => router.back()}>
+      <button className={`w-[20px] h-[20px] ${activeButton ? 'left-0' : 'absolute'}`} onClick={handleClick}>
         <ArrowIcon color="black" direction="right" className="w-[9px] h-[18px]" />
       </button>
       <h1 className={`text-t3 ${activeButton ? '' : 'w-full text-center'}`}>{title}</h1>
