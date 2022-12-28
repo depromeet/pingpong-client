ac50520254bf5013be567513e137993681cf47f3
Hwang yena <center7332@naver.com>
2022-12-28 20:45:05 +0900

    :sparkles: feat: Add env file by gitaction

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Dotenv = require('dotenv-webpack');
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
@@ -25,6 +28,7 @@ const nextConfig = {
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
+    config.plugins.push(new Dotenv({ silent: true }));
 
     return config;
   },
