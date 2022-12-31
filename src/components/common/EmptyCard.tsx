9520262667fb78a7ab87d05c4a05e34c2bb1e36a
doyoonear <doyoonleee@gmail.com>
2022-12-31 16:36:54 +0900

    :art: design: Change main check icon to black, Modify Empty Card

--- src/components/common/EmptyCard.tsx
+++ src/components/common/EmptyCard.tsx
@@ -8,7 +8,7 @@ const EmptyCard = ({ children }: { children: ReactElement | string }) => {
   return (
     <EmptyCardContainer>
       <Image src="/images/empty-img.svg" alt="empty-img" width="118" height="53" />
-      <p>{children}</p>
+      <EmptyCardText>{children}</EmptyCardText>
     </EmptyCardContainer>
   );
 };
@@ -17,18 +17,19 @@ const EmptyCardContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 3rem 8.6rem;
+  padding: 3rem;
   background-color: ${colors.gray100};
   color: ${colors.gray500};
   border-radius: 1.2rem;
+`;
 
-  p {
-    margin-top: 1.8rem;
-    display: block;
-    font-size: 1.4rem;
-    line-height: 2.1rem;
-    letter-spacing: -0.03rem;
-  }
+const EmptyCardText = styled.div`
+  margin-top: 1.8rem;
+  text-align: center;
+  display: block;
+  font-size: 1.4rem;
+  line-height: 2.1rem;
+  letter-spacing: -0.03rem;
 `;
 
 export default EmptyCard;
