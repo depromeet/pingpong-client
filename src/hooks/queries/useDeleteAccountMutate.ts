fdebfa94146d04c43ef3210a170409c52e428c7c
doyoonear <doyoonleee@gmail.com>
2023-01-01 15:41:05 +0900

    :bug: fix: Fix delete account content not updated bug

---
+++ src/hooks/queries/useDeleteAccountMutate.ts
@@ -0,0 +1,27 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { axiosClient } from '@/apis';
+
+const useDeleteAccountMutate = () => {
+  const deleteAccount = async ({ content }: { content: string }) => {
+    console.log('deleteAccount');
+    const {
+      data: { data },
+    } = await axiosClient.delete(`/members/me`, {
+      data: {
+        content,
+      },
+    });
+
+    return data;
+  };
+
+  return useMutation({
+    mutationFn: ({ content }: { content: string }) => deleteAccount({ content }),
+    onSuccess: () => {
+      // TODO: open toast with message
+    },
+  });
+};
+
+export default useDeleteAccountMutate;
