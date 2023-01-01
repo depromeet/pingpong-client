3b7ced9c282318646c246261f4e0aa9447f4f15c
doyoonear <doyoonleee@gmail.com>
2023-01-01 16:47:50 +0900

    :bug: fix: Add toast onSuccess, onError at report, delete account

--- src/hooks/queries/useDeleteAccountMutate.ts
+++ src/hooks/queries/useDeleteAccountMutate.ts
@@ -2,7 +2,11 @@ import { useMutation } from '@tanstack/react-query';
 
 import { axiosClient } from '@/apis';
 
+import { useToast } from '../useToast';
+
 const useDeleteAccountMutate = () => {
+  const { setToast } = useToast();
+
   const deleteAccount = async ({ content }: { content: string }) => {
     const {
       data: { data },
@@ -17,9 +21,8 @@ const useDeleteAccountMutate = () => {
 
   return useMutation({
     mutationFn: ({ content }: { content: string }) => deleteAccount({ content }),
-    onSuccess: () => {
-      // TODO: open toast with message
-    },
+    onSuccess: () => setToast('탈퇴 처리가 완료되었어요. 😢'),
+    onError: () => setToast('탈퇴 처리에 실패했어요.'),
   });
 };
 
--- src/hooks/queries/useReportPostMutate.ts
+++ src/hooks/queries/useReportPostMutate.ts
@@ -2,7 +2,11 @@ import { useMutation } from '@tanstack/react-query';
 
 import { axiosClient } from '@/apis';
 
+import { useToast } from '../useToast';
+
 const useReportPostMutate = () => {
+  const { setToast } = useToast();
+
   const createReportByPostId = async ({ postId, content }: { postId: number; content: string }) => {
     const {
       data: { data },
@@ -15,6 +19,8 @@ const useReportPostMutate = () => {
 
   return useMutation({
     mutationFn: ({ postId, content }: { postId: number; content: string }) => createReportByPostId({ postId, content }),
+    onSuccess: () => setToast('신고 처리가 완료되었어요. 😢'),
+    onError: () => setToast('신고 처리에 실패했어요.'),
   });
 };
 
