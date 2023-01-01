edd37109f02c84ead5f19a49beeb1bdf4cb0f5df
doyoonear <doyoonleee@gmail.com>
2023-01-01 21:21:47 +0900

    :recycle: refactor: Add recoil debugger

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,25 @@ import 'swiper/css/pagination';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import type { ReactElement } from 'react';
+import { useEffect } from 'react';
+import { RecoilRoot, useRecoilSnapshot } from 'recoil';
 
 import Layout from '@/components/common/Layout';
+
+function RecoilDebugObserver(): ReactElement | null {
+  const snapshot = useRecoilSnapshot();
+  const getLoadable = snapshot.getLoadable;
+  const snapshotList = Array.from(snapshot.getNodes_UNSTABLE({ isModified: true }));
+  useEffect(() => {
+    console.debug('The following atoms were modified:');
+    for (const node of snapshotList) {
+      console.debug(node.key, getLoadable(node));
+    }
+  }, [snapshotList, getLoadable]);
+
+  return null;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -17,6 +33,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
+        <RecoilDebugObserver />
         <div id="portal" />
         <Layout>
           <Component {...pageProps} />
