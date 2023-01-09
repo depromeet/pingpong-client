6f29f339b8d30e9df49bf8284bf90375f630b11d
L2HYUNN <donghyuni98@gmail.com>
2023-01-09 18:07:13 +0900

    :bug: fix: Fix profile edit, profile index popup issue
    프로필 카테고리 편집, 저장 완료 이후 뒤로가기 클릭시 모달이 발생하는 오류 해결

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,11 +3,14 @@ import { useRecoilState } from 'recoil';
 
 import ProfileView from '@/components/profile/ProfileView';
 import useUserInfoQuery from '@/hooks/queries/useUserInfoQuery';
+import { usePopupWithBlock } from '@/hooks/usePopupWithBlock';
 import { myInfoAtom } from '@/store/components';
 
 export default function ProfileMe() {
   const [userInfo, setUserInfo] = useRecoilState(myInfoAtom);
   const { data, isSuccess } = useUserInfoQuery();
+
+  usePopupWithBlock(undefined);
 
   useEffect(() => {
     if (isSuccess) {
