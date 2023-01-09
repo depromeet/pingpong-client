fa5ae1f62415ddee236bbfb728751c23de4c1651
doyoonear <doyoonleee@gmail.com>
2023-01-09 20:45:25 +0900

    :sparkles: feat: Add save button disabled rule

--- src/hooks/useNickname.ts
+++ src/hooks/useNickname.ts
@@ -1,17 +1,41 @@
 import type { ChangeEvent } from 'react';
 import { useCallback, useEffect, useState } from 'react';
 
-import { validateNickname } from '@/lib/utils';
+import { replaceWithRegex } from '@/lib/utils';
 
-const regex = /^[ㄱ-ㅎ|가-힣|a-z|A-Z]*$/;
+const regex = /^[ㄱ-ㅎ|가-힣|a-z|A-Z| ]*$/;
 
 const useNickname = () => {
   const [name, setName] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleNameChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setName(() => validateNickname(event.target.value));
+    setName(() => replaceWithRegex(regex, event.target.value));
   }, []);
+
+  const validateNicknameLength = (value: string) => {
+    return value.length < 2 || value.length > 10;
+  };
+
+  // TODO: validateAll 함수 개선
+  const validateAll = useCallback(() => {
+    let errMsg = '';
+
+    if (regex.test(name)) {
+      errMsg = '';
+    } else {
+      errMsg = '한글/영어만 사용이 가능해요';
+      setErrorMessage(errMsg);
+      return;
+    }
+
+    if (!validateNicknameLength(name)) {
+      errMsg = '';
+    } else {
+      errMsg = '공백 포함 2자 이상 10자 이하로만 작성해주세요';
+    }
+    setErrorMessage(errMsg);
+  }, [name, setErrorMessage]);
 
   const handleNameClear = () => {
     setName('');
@@ -19,11 +43,12 @@ const useNickname = () => {
 
   useEffect(() => {
     if (!name.length) return;
-    regex.test(name) ? setErrorMessage('') : setErrorMessage('한글/영어만 사용이 가능해요');
-  }, [name]);
+    validateAll();
+  }, [name, validateAll]);
 
   return {
     name,
+    setName,
     handleNameChange,
     handleNameClear,
     errorMessage,
