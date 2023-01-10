5220f4f472aca023fe53fdea3e8dba00636750dc
doyoonear <doyoonleee@gmail.com>
2023-01-10 11:18:49 +0900

    :bug: fix: Fix nickname redirected

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -35,6 +35,15 @@ export const useAuth = () => {
   }, [setIsLogin]);
 
   useEffect(() => {
+    if (router.asPath === '/nickname/') {
+      return;
+    }
+
+    if (isLogin && router.asPath === '/') {
+      router.replace('/main');
+      return;
+    }
+
     if (!isLogin && router.asPath !== '/') {
       router.replace('/');
       return;
